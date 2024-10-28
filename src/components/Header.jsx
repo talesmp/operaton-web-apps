a3f0db38a56c45931b720c901c4faba0cc21cf79
lambdaschmied2 <paul.hempel@lambdaschmiede.com>
2024-10-28 16:48:21 +0100

    show an introdcution

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,64 +1,61 @@
+// noinspection HtmlUnknownAnchorTarget,JSValidateTypes
+
 import { useLocation } from 'preact-iso'
 
 export function Header () {
   const { url } = useLocation()
 
-  return (
-    <header>
-      <nav id="secondary-navigation">
-        <span id="logo">Operaton BPM</span>
+  return <header>
+    <nav id="secondary-navigation">
+      <span id="logo">
+        <a href="/">Operaton BPM</a>
+      </span>
+      <menu>
         <menu>
-          <menu>
-            <li><a href="#content">Skip to content</a></li>
-            <li><a href="#primary-navigation">Skip to Primary Navigation</a>
-            </li>
-          </menu>
-          <menu>
-            <li><a href="/accessibilty">Accessibility</a></li>
-            <li><a href="/Help">Help</a></li>
-            <li>Shortcuts</li>
-          </menu>
-          <menu>
-            <li><a href="/about">About</a></li>
-            <li><a href="/settings">Settings</a></li>
-            <li><a href="/account">Account</a></li>
-          </menu>
+          <li><a href="#content">Skip to content</a></li>
+          <li><a href="#primary-navigation">Skip to Primary Navigation</a>
+          </li>
         </menu>
-      </nav>
-      <nav id="primary-navigation" aria-label="Main">
-
         <menu>
-          <menu>
-            <li>
-              <a href="/tasks" class={url.startsWith("/tasks") && "active"}>
-                Tasks
-              </a>
-            </li>
-          </menu>
-          <menu>
-            <li>
-              <a href="/processes" class={url.startsWith("/processes") && "active"}>
-                Processes
-              </a>
-            </li>
-            <li>Decisions</li>
-          </menu>
-          <menu>
-            <li>Deployments</li>
-            <li>Batches</li>
-            <li>Migrations</li>
-          </menu>
-          <menu>
-            <li>Admin</li>
-          </menu>
+          <li><a href="/accessibilty">Accessibility</a></li>
+          <li><a href="/help">Help</a></li>
+          <li><a href="/">Shortcuts</a></li>
         </menu>
+        <menu>
+          <li><a href="/about">About</a></li>
+          <li><a href="/settings">Settings</a></li>
+          <li><a href="/account">Account</a></li>
+        </menu>
+      </menu>
+    </nav>
+    <nav id="primary-navigation" aria-label="Main">
 
-        {/*
-          <a href="/404" class={url == "/404" && "active"}>
-            404
-          </a>
-          */}
-      </nav>
-    </header>
-  )
+      <menu>
+        <menu>
+          <li>
+            <a href="/tasks" class={url.startsWith('/tasks') && 'active'}>
+              Tasks
+            </a>
+          </li>
+        </menu>
+        <menu>
+          <li>
+            <a href="/processes"
+               class={url.startsWith('/processes') && 'active'}>
+              Processes
+            </a>
+          </li>
+          <li><a href="/">Decisions</a></li>
+        </menu>
+        <menu>
+          <li><a href="/">Deployments</a></li>
+          <li><a href="/">Batches</a></li>
+          <li><a href="/">Migrations</a></li>
+        </menu>
+        <menu>
+          <li><a href="/">Admin</a></li>
+        </menu>
+      </menu>
+    </nav>
+  </header>
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,56 @@
 export const Home = () => (
-  <main>
-    <h1>Operaton</h1>
-    <p>Hallo Welt</p>
+  <main class="p-3">
+    <h1>Welcome to Operaton!</h1>
+    <p>You can find general information on operaton on <a
+      href="https://operaton.org">operaton.org</a>.</p>
+
+    <h2>Help</h2>
+    <p>If you are not sure how to use Operaton or its web apps, start by taking
+      a look at our help pages:</p>
+    <ul>
+      <li><a href="/help">Help Page</a></li>
+      <li><a href="http://operaton.org/getting-started">Getting Started</a></li>
+    </ul>
+
+    <h2>Web Apps</h2>
+    <p>You are currently using the integrated web apps of Operaton.</p>
+
+    <h2>Community</h2>
+    <p>To visit the community forum to discuss features and find help. If you
+      like to contribute to the project, check out our public git
+      repository:</p>
+    <ul>
+      <li><a href="https://forum.operaton.org">Forum</a></li>
+      <li><a href="https://github.com/operaton/operaton">GitHub</a></li>
+    </ul>
+
+    <h3>Reporting a bug</h3>
+    <p>If you encountered an error while using Operaton, please create a ticket
+      to let us know:</p>
+    <ul>
+      <li>
+        <a href="https://github.com/operaton/operaton/issues">
+          Create issues on GitHub
+        </a>
+      </li>
+    </ul>
+
+    <h3>Requesting features</h3>
+    <p>If you miss some functionality while using Operaton, you can communicate
+      this as well. Please keep in mind, that features which a very specific to
+      an organization might be better added as an extension, not as part of the
+      official community code.</p>
+    <ul>
+      <li>
+        <a href="https://forum.operaton.org">
+          Discuss feature ideas on the forum
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/operaton/operaton/issues">
+          Create a feature request on GitHub
+        </a>
+      </li>
+    </ul>
   </main>
-);
+)

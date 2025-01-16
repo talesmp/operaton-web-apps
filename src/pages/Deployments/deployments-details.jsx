02645d471900ba6598b0431d7cbb1f235b23340a
Anton Wildermut <anton.wildermut@echte-berater.de>
2025-01-16 19:41:12 +0100

    add modal for deletion

--- src/pages/Deployments/deployments-details.jsx
+++ src/pages/Deployments/deployments-details.jsx
@@ -10,6 +10,11 @@ const DeploymentDetails = ({ selectedDeployment }) => {
     const [diagramXml, setDiagramXml] = useState(null);
     const diagramContainerRef = useRef(null);
 
+    const [showModal, setShowModal] = useState(false);
+    const [cascade, setCascade] = useState(false);
+    const [skipCustomListeners, setSkipCustomListeners] = useState(true);
+    const [skipIoMappings, setSkipIoMappings] = useState(true);
+
     useEffect(() => {
         if (selectedDeployment?.definition?.id) {
             api.get_diagram(state, selectedDeployment.definition.id)
@@ -29,9 +34,17 @@ const DeploymentDetails = ({ selectedDeployment }) => {
                 breadcrumbs.remove();
             }
         }
-    }, [diagramXml]); 
+    }, [diagramXml]);
 
-        
+    const handleDelete = () => {
+        delete_deployment(state, selectedDeployment.definition.deploymentId, {
+            cascade,
+            skipCustomListeners,
+            skipIoMappings,
+        });
+        setShowModal(false);
+    };
+
     if (!selectedDeployment) {
         return (
             <div class="deployments-details-panel deployments-details">
@@ -57,24 +70,87 @@ const DeploymentDetails = ({ selectedDeployment }) => {
                         <span class="status-active">Active</span>
                     </p>
                 )}
-                <p> <strong>Process Definition Key: </strong>{selectedDeployment.definition.key}</p>
                 <p><strong>Version: </strong>{selectedDeployment.definition.version}</p>
+                <p> <strong>Process Definition Key: </strong>{selectedDeployment.definition.key}</p>
                 <p><strong>Deployment ID: </strong>{selectedDeployment.definition.deploymentId}</p>
                 {selectedDeployment.definition.startableInTasklist ? (
                     <p><strong>Startable in Tasklist: </strong>True</p>
                 ) : (
                     <p><strong>Startable in Tasklist: </strong>False</p>
                 )}
-                <button 
-                    onClick={() => delete_deployment(state, selectedDeployment.definition.deploymentId, {
-                        // TODO: ask user for params in UI
-                        cascade: true,
-                        skipCustomListeners: true,
-                        skipIoMappings: true,
-                    })}
-                    class="delete-button"><Icons.trash /> Delete Deployment
+
+                <button onClick={() => setShowModal(true)} class="delete-button">
+                    <Icons.trash /> Delete Deployment
                 </button>
             </div>
+            {showModal && (
+                <div class="modal-overlay">
+                    <div class="modal-content">
+                        <div class="title">
+                            <h3>Delete Deployment: {selectedDeployment.definition.name}</h3>
+                        </div>
+                        <div class="modal-body">
+                            <hr />
+                            <form style="display: flex; flex-direction: column; gap: var(--spacing-2);">
+                                <div className="form-group">
+                                    <input
+                                        type="checkbox"
+                                        checked={cascade}
+                                        onChange={(e) => setCascade(e.target.checked)}
+                                    />
+                                    <label>
+                                        Cascade
+                                        <span>If the value is enabled, all instances, including historic Instances, related to this deployment will also be deleted.</span>
+                                    </label>
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="checkbox"
+                                        checked={skipCustomListeners}
+                                        onChange={(e) => setSkipCustomListeners(e.target.checked)}
+                                    />
+                                    <label>
+                                        Skip Custom Listeners
+                                        <span>If the value is enabled, only built-in listeners will be notified with the end event.</span>
+                                    </label>
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="checkbox"
+                                        checked={skipIoMappings}
+                                        onChange={(e) => setSkipIoMappings(e.target.checked)}
+                                    />
+                                    <label>
+                                        Skip IO Mappings
+                                        <span>If the value is enabled IO
+                                            mappings will be skipped during deployment removal.</span>
+                                    </label>
+                                </div>
+                            </form>
+                            <hr />
+                        </div>
+                        <div class="modal-footer">
+                            <p class="modal-footer-hint">Are you sure you want to permanently delete the given deployment?</p>
+
+                            <div class="modal-footer-buttons">
+                                <button
+                                    class="btn btn-secondary"
+                                    onClick={() => setShowModal(false)} 
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    class="btn btn-primary"
+                                    onClick={handleDelete}
+                                >
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+
             <div class="diagram-content" ref={diagramContainerRef}>
                 {diagramXml ? (
                     <ReactBpmn

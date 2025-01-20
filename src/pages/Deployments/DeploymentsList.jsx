96a43b2a5f1e2bbd7693557ce740cad3d3aa385f
Anton Wildermut <anton.wildermut@echte-berater.de>
2025-01-20 23:04:48 +0100

    add sorting

--- src/pages/Deployments/DeploymentsList.jsx
+++ src/pages/Deployments/DeploymentsList.jsx
@@ -1,22 +1,48 @@
-import { useContext } from 'preact/hooks';
-import { AppState } from '../../state.js'
-import { get_deployment_resources } from '../../api.js';
+import { useContext, useState } from "preact/hooks";
+import { AppState } from "../../state.js";
+import { get_deployment_resources } from "../../api.js";
+import * as Icons from '../../assets/icons.jsx'
 
 export const DeploymentsList = () => {
-  const state = useContext(AppState)
-  
+  const state = useContext(AppState);
+  const [isAscending, setIsAscending] = useState(true);
+
+  const sortDeploymentsByTime = () => {
+    state.deployments.value.sort((a, b) => {
+      const timeA = new Date(a.deploymentTime).getTime();
+      const timeB = new Date(b.deploymentTime).getTime();
+      return isAscending ? timeA - timeB : timeB - timeA;
+    });
+  };
+
   return (
     <div class="deployments-list list-scrollable">
       <h1 class="title">Deployments</h1>
+      {/**
+       * sort button
+       * <button
+        onClick={() => {
+          setIsAscending(!isAscending);
+          sortDeploymentsByTime();
+        }}
+        class="sort-button"
+        title={`Sort by Time (${isAscending ? "Ascending" : "Descending"})`}
+      >
+        Sort by Time {isAscending ? <Icons.chevron_up /> : <Icons.chevron_down/>}
+      </button>
+       */}
+
       <ul class="tile-list">
         {state.deployments.value.map((deployment) => (
           <li
             key={deployment.id}
-            class={`tile-item ${state.selected_deployment.value?.id === deployment.id ? "selected" : ""}`}
+            class={`tile-item ${
+              state.selected_deployment.value?.id === deployment.id ? "selected" : ""
+            }`}
             onClick={() => {
-              resetSelectedDetails(state); 
-              state.selected_deployment.value = deployment; 
-              get_deployment_resources(state, deployment.id)
+              resetSelectedDetails(state);
+              state.selected_deployment.value = deployment;
+              get_deployment_resources(state, deployment.id);
             }}
           >
             <div class="padding-1">
@@ -27,6 +53,7 @@ export const DeploymentsList = () => {
                   <span class="title">N/A</span>
                 )}
               </header>
+              <div>{deployment?.deploymentTime}</div>
             </div>
           </li>
         ))}

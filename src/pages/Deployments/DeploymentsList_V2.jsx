11038ddd01f2c30aae11440a57bc781045a8fe18
Anton Wildermut <anton.wildermut@echte-berater.de>
2025-01-22 20:26:38 +0100

    -

--- src/pages/Deployments/DeploymentsList_V2.jsx
+++ src/pages/Deployments/DeploymentsList_V2.jsx
@@ -1,36 +1,50 @@
 import { useContext, useState } from 'preact/hooks'
 import { AppState } from '../../state.js'
-import { get_deployment_resources } from '../../api.js'
+import { get_deployment_resources, get_deployment_v2 } from '../../api.js'
+import { signal } from '@preact/signals'
+
+const deployments = signal(null)
 
 export const DeploymentsList_V2 = () => {
   const state = useContext(AppState)
 
+
+  if(deployments.value == null){
+    get_deployment_v2(state).then((response) => deployments.value = response).then(() => console.log(deployments))
+  }
+
+
   return (
-    <div class="deployments-list list-scrollable">
-      <h3 class="screen-hidden">Queried deployments</h3>
-      <ul class="tile-list">
-        {state.deployments.value.map((deployment) => (
-          <li
-            key={deployment.id}
-            class={`tile-item ${
-              state.selected_deployment.value?.id === deployment.id ? 'selected' : ''
-            }`}
-            onClick={() => {
-              resetSelectedDetails(state)
-              state.selected_deployment.value = deployment
-              get_deployment_resources(state, deployment.id)
-            }}
-          >
-            <a>
-              <div className="title">
-                {deployment?.name
-                  ? deployment?.name
-                  : deployment?.id}</div>
-              <span>{deployment?.deploymentTime}</span>
-            </a>
-          </li>
-        ))}
-      </ul>
+    <div>
+       {deployments.value ? (
+          <div class="deployments-list list-scrollable">
+          <h3 class="screen-hidden">Queried deployments</h3>
+          <ul class="tile-list">
+            {deployments.value.map((deployment) => (
+              <li
+                key={deployment.id}
+                class={`tile-item ${state.selected_deployment.value?.id === deployment.id ? 'selected' : ''
+                  }`}
+                onClick={() => {
+                  //resetSelectedDetails(state)
+                  state.selected_deployment.value = deployment
+                  get_deployment_resources(state, deployment.id)
+                }}
+              >
+                <a>
+                  <div className="title">
+                    {deployment?.name
+                      ? deployment?.name
+                      : deployment?.id}</div>
+                  <span>{deployment?.deploymentTime}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        ) : (
+          <p>Loading deployments...</p>
+        )}
     </div>
   )
 }

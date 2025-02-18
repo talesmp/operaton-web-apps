918ac4d52672afb42b101c29ee1282e458454f70
Anton Wildermut <anton.wildermut@echte-berater.de>
2025-02-18 09:23:58 +0100

    add decision list

--- src/pages/Decisions.jsx
+++ src/pages/Decisions.jsx
@@ -14,10 +14,39 @@ const DecisionsPage = () => {
   return (
     <main class="fade-in list-container">
       <h2 class="screen-hidden">Decisions</h2>
-      <p>huhu</p>
+      <DecisionsList/>
     </main>
   )
 }
 
+
+const DecisionsList = () => {
+  const
+    state = useContext(AppState),
+    { params } = useRoute()
+
+  return (
+    <div class="list-wrapper">
+      <h3 class="screen-hidden">Queried decisions</h3>
+      <ul class="list">
+        {state.decisions.value?.map((decision) => (
+          <li
+            key={decision.id}
+            class={params.deployment_id === decision.id ? 'selected' : null}
+          >
+            <a href={`/deployments/${decision.id}`}>
+              <div class="title">
+                {decision?.name || decision?.id}
+              </div>
+              <footer>
+                <time datetime={decision.deploymentTime}>{new Date(decision.deploymentTime).toLocaleDateString()}</time>
+              </footer>
+            </a>
+          </li>
+        )) ?? 'Loading...'}
+      </ul>
+    </div>
+  )
+}
 
 export { DecisionsPage }
